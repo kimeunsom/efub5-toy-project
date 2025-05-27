bcdc7a2f786c3002ceac5cccf828383fc532a540
cotton <72211340+kimeunsom@users.noreply.github.com>
2025-05-27 16:11:57 +0900

    feat : 파일 구조 정리 - mvc 패턴 디렉토리 설정

---
+++ efubtoy/src/main/java/com/example/efubtoy/EfubtoyApplication.java
@@ -0,0 +1,13 @@
+package com.example.efubtoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfubtoyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfubtoyApplication.class, args);
+	}
+
+}
---
+++ efubtoy/src/test/java/com/example/efubtoy/EfubtoyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.efubtoy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfubtoyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
