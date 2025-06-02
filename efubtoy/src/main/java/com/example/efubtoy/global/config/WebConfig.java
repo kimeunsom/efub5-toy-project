5c6f42274e9757cd73f50c7c6b4387af63e3a8c5
cotton <72211340+kimeunsom@users.noreply.github.com>
2025-06-03 02:44:58 +0900

    feat : config 프론트 배포 주소 수정

--- efubtoy/src/main/java/com/example/efubtoy/global/config/WebConfig.java
+++ efubtoy/src/main/java/com/example/efubtoy/global/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://toy-project-chi.vercel.app", "https://busanlottegiants.p-e.kr")
+                .allowedOrigins("http://localhost:5173", "https://toy-project-chi.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
