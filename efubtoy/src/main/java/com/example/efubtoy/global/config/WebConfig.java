bd2603365dea1c55fe8f4188ad7d3835881f75b8
cotton <72211340+kimeunsom@users.noreply.github.com>
2025-06-03 01:04:10 +0900

    feat : config CORS 처리 부분 복원

--- efubtoy/src/main/java/com/example/efubtoy/global/config/WebConfig.java
+++ efubtoy/src/main/java/com/example/efubtoy/global/config/WebConfig.java
@@ -9,14 +9,11 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        // 이 메서드 전체를 주석 처리하거나 삭제합니다.
-        /*
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173", "https://busanlottegiants.p-e.kr")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
-        */
     }
 }
